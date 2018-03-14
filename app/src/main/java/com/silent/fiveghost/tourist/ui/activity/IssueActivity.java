9d423458883a6bfda17e42b0b1fd90924293dc89
funChen <13521473277@163.com>
2018-03-14 20:05:27 +0800

    chensl:一站式发布页面功能开发；

--- app/src/main/java/com/silent/fiveghost/tourist/ui/activity/IssueActivity.java
+++ app/src/main/java/com/silent/fiveghost/tourist/ui/activity/IssueActivity.java
@@ -1,9 +1,20 @@
 package com.silent.fiveghost.tourist.ui.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.silent.fiveghost.tourist.R;
 import com.silent.fiveghost.tourist.ui.BaseActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * @author FunChen
@@ -13,15 +24,65 @@ import com.silent.fiveghost.tourist.ui.BaseActivity;
  * <p>
  * ${TAGS}
  */
-public class IssueActivity extends BaseActivity {
+public class IssueActivity extends BaseActivity implements View.OnClickListener{
+
+	@BindView(R.id.iv_back_all)
+	ImageView ivBackAll;
+	@BindView(R.id.tv_title_all)
+	TextView tvTitleAll;
+	@BindView(R.id.iv_message_all)
+	ImageView ivMessageAll;
+	@BindView(R.id.tv_issue_one_state)
+	TextView tvIssueOneState;
+	@BindView(R.id.tv_issue_sub_state)
+	TextView tvIssueSubState;
+	@BindView(R.id.ll_issue_tab)
+	LinearLayout llIssueTab;
+	@BindView(R.id.tv_issue_text_go)
+	TextView tvIssueTextGo;
+	@BindView(R.id.img_issue_icon_go)
+	ImageView imgIssueIconGo;
+	@BindView(R.id.rl_issue_go)
+	RelativeLayout rlIssueGo;
+	@BindView(R.id.rv_issue_go_city_list)
+	RecyclerView rvIssueGoCityList;
+	@BindView(R.id.tv_issue_text_to)
+	TextView tvIssueTextTo;
+	@BindView(R.id.img_issue_icon_to)
+	ImageView imgIssueIconTo;
+	@BindView(R.id.rl_issue_to)
+	RelativeLayout rlIssueTo;
+	@BindView(R.id.rv_issue_to_city_list)
+	RecyclerView rvIssueToCityList;
+	@BindView(R.id.btn_issue_submit)
+	Button btnIssueSubmit;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_issue);
+		ButterKnife.bind(this);
 
+		initView();
 
+	}
 
+	private void initView() {
+		btnIssueSubmit.setOnClickListener(this);
+	}
 
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+			case R.id.btn_issue_submit:
+
+//				startActivity(new Intent(IssueActivity.this, .class));
+				break;
+
+			default:
+
+				break;
+
+		}
 	}
 }
---
+++ app/src/main/java/com/silent/fiveghost/tourist/ui/activity/IssueOneStepActivity.java
@@ -0,0 +1,22 @@
+package com.silent.fiveghost.tourist.ui.activity;
+
+import android.os.Bundle;
+
+import com.silent.fiveghost.tourist.R;
+import com.silent.fiveghost.tourist.ui.BaseActivity;
+
+/**
+ * @author FunChen
+ * @ClassName: ${TYPE_NAME}
+ * @Description: ${TODO}(一站)
+ * @date 2018/3/14 19:59
+ * <p>
+ * ${TAGS}
+ */
+public class IssueOneStepActivity extends BaseActivity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_issue_one_step);
+	}
+}
