38f55449e4e33aa2563913bcafa501905a4bb15b
lenovo <3262674949@qq.com>
2018-03-19 02:29:55 +0800

    订单内容

--- app/src/main/java/com/silent/fiveghost/tourist/ui/activity/OrderContentActivity.java
+++ app/src/main/java/com/silent/fiveghost/tourist/ui/activity/OrderContentActivity.java
@@ -149,6 +149,7 @@ public class OrderContentActivity extends BaseActivity implements View.OnClickLi
         tv_money = (TextView) findViewById(R.id.tv_money);
         submit_orders_order = (Button) findViewById(R.id.submit_orders_order);
 
+
         bt_birthday.setOnClickListener(this);
         submit_orders_order.setOnClickListener(this);
     }
